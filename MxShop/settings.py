fb60203fc9f783e19c4bb9400ae8ffbfec8fff86
tqtl911@163.com <19930911cXS>
2018-10-02 17:43:29 +0800

    goods.models.py的设计

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
+    'users',
+    'user_operation',
+    'trade',
+    'goods',
 ]
 
 MIDDLEWARE = [
